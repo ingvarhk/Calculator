7011fb8cd74d9d9af58af8d60335c1b75385ce0c
HikBit <ingvar@hahnkristensen.dk>
2019-10-23 20:45:50 +0200

    Update HIKcalc.py

--- HIKcalc.py
+++ HIKcalc.py
@@ -1,6 +1,6 @@
 import math
 
-print("WELCOME TO HIKcalc!!")
+print("Welcome to HIKcalc!!")
 print("-----------------------------------------------")
 print("See commands and examples by typing 'help'")
 print("")
@@ -40,33 +40,85 @@ def power(num1, num2):
     return returnSum
 
 def userHelp():
-    print("""COMMANDS
-# plus -- Plus two numbers with each other
-# min -- Minus a number with a number
-# mult -- Multiply numbers
-# divd -- Divide numbers
-# sqrt -- Get sqareroot of number
-# pow -- Get det power of number
+    print("""
+CALCULATOR COMMANDS
+# plus -- Plus two numbers with each other. Takes 2 arguments.
+# min -- Minus a number with a number. Takes 2 arguments.
+# mult -- Multiply numbers. Takes 2 arguments.
+# divd -- Divide numbers. Takes 2 arguments.
+# sqrt -- Get sqareroot of number. Takes 1 argument.
+# pow -- Get det power of number. Takes 2 arguments.
+# deci -- End your command with 'deci' followed by how many decimals you want. Default is 2.
 
 EXAMPLES
 # Input: 'plus 40 5'
 # Output: '45'
-#
-# Input: 'divd 34 10'
-# Output: '3.4'
-#
+
+# Input: 'divd 243 34 deci 4'
+# Output: '7.1471'
+
 # Input: 'pow 4 2'
 # Output: '16'
 
-DOCUMENTATION
+OTHER COMMANDS
+# exit -- Exit HIKcalc
+# help -- List of commands and examples
+
+SEE MORE
+# http://ingvar.hahnkristensen.dk/hikcalc
+# https://github.com/HikBit/HIKcalc
 """)
 
 
+#for x in range(0, 1):
 while True:
     userInput = input("> ")
     
     if userInput == "help":
         userHelp()
+        
+    elif userInput.startswith('plus '):
+        userInput = userInput.replace('plus ', '', 1)
+        
+        try:
+            numbers = userInput.split(' ')
+                
+            if len(numbers) != 2:
+                print("ERROR: Make sure you are not using any letters. This command can only take 2 arguments")
+            else:
+                i = 0
+                
+                for number in numbers:      
+                    if i == 0:
+                        num1 = float(number)
+                        i =+ 1
+                    else:
+                        num2 = float(number)
+                print("RESULT: " + str(round(addition(num1, num2))))
+        except Exception as e:
+            print(e)
+        
+    elif userInput.startswith('min '):
+        userInput = userInput.replace('min ', '', 1)
+        
+        try:
+            numbers = userInput.split(' ')
+                
+            if len(numbers) != 2:
+                print("ERROR: Make sure you are not using any letters. This command can only take 2 arguments")
+            else:
+                i = 0
+                
+                for number in numbers:      
+                    if i == 0:
+                        num1 = float(number)
+                        i =+ 1
+                    else:
+                        num2 = float(number)
+                print("RESULT: " + str(round(subtraction(num1, num2))))
+        except Exception as e:
+            print(e)
+        
 
 
 
